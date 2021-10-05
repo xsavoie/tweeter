2d6f4a81ff37a7f19266eebfa54a558a3a788ab8
Xavier Savoie <xaviersavoie@gmail.com>
2021-10-05 21:49:15 +0000

    refactored and cleaned up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     $('.counter').text(140 - $(this).val().length);
     if ($('.counter').text() <= 0) {
       return $('.counter').addClass('too-many-chars');
-    } 
+    }
     if ($('.counter').text() > 0) {
       return $('.counter').removeClass('too-many-chars');
     }
-  })
+  });
 });
