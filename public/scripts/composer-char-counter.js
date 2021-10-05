aae759b3ec032c298d76c3e184c5b6bc83ef6f2f
Xavier Savoie <xaviersavoie@gmail.com>
2021-10-05 21:41:58 +0000

    Added area for submitted tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
   $('#tweet-text').keyup(function() {
     $('.counter').text(140 - $(this).val().length);
     if ($('.counter').text() <= 0) {
-      $('.counter').addClass('too-many-chars')
-    } else if ($('.counter').text() > 0) {
-      $('.counter').removeClass('too-many-chars')
+      return $('.counter').addClass('too-many-chars');
+    } 
+    if ($('.counter').text() > 0) {
+      return $('.counter').removeClass('too-many-chars');
     }
   })
 });
